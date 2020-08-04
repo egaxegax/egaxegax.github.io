993eda87c2d52fcaad40ced2cd3c1491e4171b32
egax <egax@bk.ru>
2020-08-05 01:35:15 +0300

    +fotos from FotoSite

---
+++ fotos/index.js
@@ -0,0 +1,23 @@
+ALBUMS={
+  "0": "Мой город",
+  "1": "Бутово",
+  "2": "Природа",
+  "3": "Пейзажи",
+  "4": "Битцевский парк"
+};
+IMGURLS=[
+  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15860849431879566631;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Вид на Ленинскую слободу"],
+  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15926717771737704523;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Граффити в Зюзино"],
+  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15551941700000000819;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Граффити у Ленинской слободы"],
+  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15552671610000000369;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Граффити в Бутово"],
+  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15926717771737704523;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Кусты розы у дома"],
+  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15860849431879566631;0;3&exif=1&full=1&x-email=egax%40bk.ru", "Апрель"],
+  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15691933061314805357;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Большой пруд в Захарьино"],
+  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15926717771737704523;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Рябина у школы"],
+  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15691933061314805357;0;3&exif=1&full=1&x-email=egax%40bk.ru", "У новой школы в Захарьино"],
+  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15350168360000000271;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Сентябрь в Бутово"],
+  ["2", "https://apf.mail.ru/cgi-bin/readmsg?id=15570908410627523780;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Квадратное облако"],
+  ["3", "https://apf.mail.ru/cgi-bin/readmsg?id=15386419440000000623;0;7&exif=1&full=1&x-email=egax%40bk.ru", "Весна пришла"],
+  ["4", "https://apf.mail.ru/cgi-bin/readmsg?id=15798929980266298833;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Зима в парке"],
+  ["4", "https://apf.mail.ru/cgi-bin/readmsg?id=15570908410627523780;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Осень в парке"]
+];
