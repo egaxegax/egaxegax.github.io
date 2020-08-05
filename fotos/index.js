9f73ba95fc4dfc790c2082a6c9b4651282650097
egax <egax@bk.ru>
2020-08-06 00:18:36 +0300

    +fotos from FotoSite

--- fotos/index.js
+++ fotos/index.js
@@ -6,18 +6,17 @@ ALBUMS={
   "4": "Битцевский парк"
 };
 IMGURLS=[
-  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15860849431879566631;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Вид на Ленинскую слободу"],
-  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15926717771737704523;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Граффити в Зюзино"],
-  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15551941700000000819;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Граффити у Ленинской слободы"],
-  ["0", "https://apf.mail.ru/cgi-bin/readmsg?id=15552671610000000369;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Граффити в Бутово"],
-  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15926717771737704523;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Кусты розы у дома"],
-  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15860849431879566631;0;3&exif=1&full=1&x-email=egax%40bk.ru", "Апрель"],
-  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15691933061314805357;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Большой пруд в Захарьино"],
-  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15926717771737704523;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Рябина у школы"],
-  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15691933061314805357;0;3&exif=1&full=1&x-email=egax%40bk.ru", "У новой школы в Захарьино"],
-  ["1", "https://apf.mail.ru/cgi-bin/readmsg?id=15350168360000000271;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Сентябрь в Бутово"],
-  ["2", "https://apf.mail.ru/cgi-bin/readmsg?id=15570908410627523780;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Квадратное облако"],
-  ["3", "https://apf.mail.ru/cgi-bin/readmsg?id=15386419440000000623;0;7&exif=1&full=1&x-email=egax%40bk.ru", "Весна пришла"],
-  ["4", "https://apf.mail.ru/cgi-bin/readmsg?id=15798929980266298833;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Зима в парке"],
-  ["4", "https://apf.mail.ru/cgi-bin/readmsg?id=15570908410627523780;0;1&exif=1&full=1&x-email=egax%40bk.ru", "Осень в парке"]
+  ["0", "https://content-27.foto.my.mail.ru/bk/egax/37/b-85.jpg", "Вид на Ленинскую слободу"],
+  ["0", "https://content-13.foto.my.mail.ru/bk/egax/37/b-87.jpg", "Клумба на Восточной"],
+  ["0", "https://content-30.foto.my.mail.ru/bk/egax/37/b-88.JPG", "Стадион Торпедо"],
+  ["1", "https://content-29.foto.my.mail.ru/bk/egax/37/b-86.jpg", "Кусты розы у дома"],
+  ["1", "https://content-30.foto.my.mail.ru/bk/egax/37/b-89.jpg", "У новой школы в Захарьино"],
+  ["1", "https://content-24.foto.my.mail.ru/bk/egax/37/b-91.JPG", "Пруд у церкви в Захарьино"],
+  ["1", "https://content-4.foto.my.mail.ru/bk/egax/37/b-90.JPG", "Улица в Захарьино осенью"],
+  ["2", "https://content-28.foto.my.mail.ru/bk/egax/37/b-97.JPG", "Бабочка"],
+  ["3", "https://content-1.foto.my.mail.ru/bk/egax/37/b-98.JPG", "Осень пришла"]
+  ["4", "https://content-20.foto.my.mail.ru/bk/egax/37/b-92.JPG", ""],
+  ["4", "https://content-6.foto.my.mail.ru/bk/egax/37/b-93.JPG", ""],
+  ["4", "https://content-18.foto.my.mail.ru/bk/egax/37/b-95.JPG", ""],
+  ["4", "https://content-14.foto.my.mail.ru/bk/egax/37/b-94.JPG", ""],
 ];
