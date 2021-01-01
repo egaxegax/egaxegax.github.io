6ac22292d78db6226d2c6c29229d83b9cf300ae7
egax <egax@ya.ru>
2021-01-01 17:48:00 +0300

    songs++

--- index.py
+++ index.py
@@ -41,9 +41,9 @@ for root, dirs, files in os.walk(path, topdown=False):
       ftime = os.path.getmtime(os.path.join(root, name))
       im = Image.open(os.path.join(root, name))
       im.thumbnail((100,100))
-      if not os.path.exists(os.path.join(root, '_th')):
-        os.mkdir(os.path.join(root, '_th'))
-      im.save(os.path.join(root, '_th', fname+'.jpeg') , "JPEG")
+      if not os.path.exists(os.path.join(root, 'th')):
+        os.mkdir(os.path.join(root, 'th'))
+      im.save(os.path.join(root, 'th', fname+'.jpeg') , "JPEG")
     elif cwd in ('books','posts','songs') and ext in ('.txt'):
       ftime = os.path.getmtime(os.path.join(root, name))
       if fname != 'about':
