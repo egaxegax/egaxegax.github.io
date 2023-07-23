db22e8005990fc22b2b45f4f8a4161b186da4851
egax <egax@ya.ru>
2023-07-23 17:08:31 +0300

    +reklama yandex rtb

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -55,5 +55,6 @@ if(!String(window.location).match(/file:|localhost|127.0.1.1/)){
    (window, document, "script", "//yandex.ru/ads/system/context.js", "yaContextCb");
   window.yaContextCb.push(()=>{Ya.Context.AdvManager.render({"blockId": "R-A-2277013-1","renderTo": "yandex_rtb_R-A-2277013-1"});});
   window.yaContextCb.push(()=>{Ya.Context.AdvManager.render({"blockId": "R-A-2277013-2","renderTo": "yandex_rtb_R-A-2277013-2"});});
+  window.yaContextCb.push(()=>{Ya.Context.AdvManager.render({"blockId": "R-A-2277013-3","renderTo": "yandex_rtb_R-A-2277013-3"});});
 }
 }
