39237be1aca24ce6f144275465d23e356ddc02cf
egax <egax@ya.ru>
2023-10-10 00:12:13 +0300

    *ads size +recomend foto

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -3,15 +3,15 @@
 //
 if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
 { // liveinternet metrics
-  (function(d,s){if(d.getElementById("licntADF8"))d.getElementById("licntADF8").src="//counter.yadro.ru/hit?t45.12;r"+escape(d.referrer)+
-  ((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+(s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
-  ";h"+escape(d.title.substring(0,150))+";"+Math.random();})(document,screen);
+  (function(d,s){if(d.getElementById('licntADF8'))d.getElementById('licntADF8').src='//counter.yadro.ru/hit?t45.12;r'+escape(d.referrer)+
+  ((typeof(s)=='undefined')?'':';s'+s.width+'*'+s.height+'*'+(s.colorDepth?s.colorDepth:s.pixelDepth))+';u'+escape(d.URL)+
+  ';h'+escape(d.title.substring(0,150))+';'+Math.random();})(document,screen);
 }
 { // yandex metrics
   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments);};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0];k.async=1;k.src=r;a.parentNode.insertBefore(k,a);})
-   (window, document, "script", "//mc.yandex.ru/metrika/tag.js", "ym");
-   ym(65044687, "init", {
+   (window, document, 'script', '//mc.yandex.ru/metrika/tag.js', 'ym');
+   ym(65044687, 'init', {
      clickmap:true,
      trackLinks:true,
      accurateTrackBounce:true
@@ -31,18 +31,18 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
 //      var options = {project: 7715588};
 //      try{w.top100Counter = new top100(options);} catch(e){}
 //    });
-//    var n = d.getElementsByTagName("script")[0],
-//    s = d.createElement("script"),
+//    var n = d.getElementsByTagName('script')[0],
+//    s = d.createElement('script'),
 //    f = function () { n.parentNode.insertBefore(s, n); };
-//    s.type = "text/javascript";
+//    s.type = 'text/javascript';
 //    s.async = true;
 //    s.src = 
-//    (d.location.protocol == "https:" ? "https:" : "http:") +
-//    "//st.top100.ru/top100/top100.js";
-//    if (w.opera == "[object Opera]") {
-//      d.addEventListener("DOMContentLoaded", f, false);
+//    (d.location.protocol == 'https:' ? 'https:' : 'http:') +
+//    '//st.top100.ru/top100/top100.js';
+//    if (w.opera == '[object Opera]') {
+//      d.addEventListener('DOMContentLoaded', f, false);
 //    } else { f(); }
-//  })(window, document, "_top100q");
+//  })(window, document, '_top100q');
 //}
 //{ // google adsense
 //  (function(m,e,t,r,i,k,a){m[i]=m[i]||[];
@@ -52,7 +52,7 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
 { // reklama Yandex.RTB
   (function(m,e,t,r,i,k,a){m[i]=m[i]||[];
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0];k.async=1;k.src=r;a.parentNode.insertBefore(k,a);})
-   (window, document, "script", "//yandex.ru/ads/system/context.js", "yaContextCb");
+   (window, document, 'script', '//yandex.ru/ads/system/context.js', 'yaContextCb');
   window.adjustRtbHeight = function(){ // fix Yandex.RTB block height
     var elem, hsum = 0;
     [1,2,3].map(function(i){
@@ -62,10 +62,11 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
       }
     });
     return hsum;
-  }
+  };
   window.addYaRTB_Block = function(n,p_dark){ 
     if(document.getElementById('yandex_rtb_R-A-2277013-'+n)) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme: p_dark, blockId:'R-A-2277013-'+n, renderTo:'yandex_rtb_R-A-2277013-'+n});});
-  }
+    if(document.getElementById('yandex_rtb_C-A-2277013-'+n)) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme: p_dark, blockId:'C-A-2277013-'+n, renderTo:'yandex_rtb_C-A-2277013-'+n});});
+  };
   addYaRTB_Block(1);
   addYaRTB_Block(2);
   addYaRTB_Block(3);
@@ -74,8 +75,8 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
   var p_top = Number((sp.getAttribute('data-top') == null) ? 0 : sp.getAttribute('data-top'));
   var p_full = Number((sp.getAttribute('data-full') == null) ? 0 : sp.getAttribute('data-full'));
   console.log('p_full', p_full, 'p_floor', p_floor, 'p_top', p_top);
-  if(p_floor) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: "R-A-2277013-5", type: "floorAd"});});
-  if(p_top) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: "R-A-2277013-7", type: "topAd"});});
-  if(p_full) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: "R-A-2277013-8", type: "fullscreen", platform: "touch"});});
+  if(p_floor) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: 'R-A-2277013-5', type: 'floorAd'});});
+  if(p_top) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: 'R-A-2277013-7', type: 'topAd'});});
+  if(p_full) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: 'R-A-2277013-8', type: 'fullscreen', platform: 'touch'});});
 }
 }
