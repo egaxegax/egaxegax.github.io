5de4c52f2b559871328d5de2907724a814104ff9
egax <egax@ya.ru>
2023-10-13 00:58:26 +0300

    *ads size

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -55,12 +55,13 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
    (window, document, 'script', '//yandex.ru/ads/system/context.js', 'yaContextCb');
   window.YA_BLOCKS = {1:1, 2:2, 3:3, 4:9, 5:10, 6:11};
   window.addYaRTB_Block = function(blid,p_dark){ if(document.getElementById('yandex_rtb_'+blid)) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme: p_dark, blockId:blid, renderTo:'yandex_rtb_'+blid});}); };
-  for(var n in YA_BLOCKS) { addYaRTB_Block('R-A-2277013-'+YA_BLOCKS[n]); }
   var sp = [].slice.call(document.getElementsByTagName('script')).filter(function(s){return s.src.indexOf('metrics.js')>-1;})[0];
+  var p_dark = Number((sp.getAttribute('data-dark') == null) ? 0 : sp.getAttribute('data-dark'));
   var p_floor = Number((sp.getAttribute('data-floor') == null) ? 0 : sp.getAttribute('data-floor'));
   var p_top = Number((sp.getAttribute('data-top') == null) ? 0 : sp.getAttribute('data-top'));
   var p_full = Number((sp.getAttribute('data-full') == null) ? 0 : sp.getAttribute('data-full'));
-  console.log('p_full', p_full, 'p_floor', p_floor, 'p_top', p_top);
+  console.log('p_dark', p_dark, 'p_floor', p_floor, 'p_top', p_top, 'p_full', p_full);
+  for(var n in YA_BLOCKS) { addYaRTB_Block('R-A-2277013-'+YA_BLOCKS[n]); }
   if(p_floor) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: 'R-A-2277013-5', type: 'floorAd'});});
   if(p_top) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: 'R-A-2277013-7', type: 'topAd'});});
   if(p_full) window.yaContextCb.push(function(){Ya.Context.AdvManager.render({blockId: 'R-A-2277013-8', type: 'fullscreen', platform: 'touch'});});
