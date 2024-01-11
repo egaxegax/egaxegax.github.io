74d2b95a9da66e7b7a03744d9724b18fddd93402
egax <egax@ya.ru>
2024-01-11 23:29:35 +0300

    *reklama.html *ads -setInterval

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -56,13 +56,11 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
   window.YA_RTB = {1:1, 2:2, 3:3, 4:9, 5:10, 6:11};
   window.TDS = [];
   window.addYaRTB_Block = function(blid,p_dark,rtbid){ 
-    function ads(el){ if((el=document.getElementById('yandex_rtb_'+blid))){ 
-      window.yaContextCb.push(function(){
-        el.innerHTML = '<div style="height:'+el.offsetHeight+'px;width:'+el.offsetWidth+'px">'+el.innerHTML+'</div>';
-        Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-2277013-'+(rtbid||blid), renderTo:'yandex_rtb_'+blid});});
+    function ads(){ if(document.getElementById('yandex_rtb_'+blid)){ 
+      window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-2277013-'+(rtbid||blid), renderTo:'yandex_rtb_'+blid});});
     }};
-    while(TDS.length) clearInterval(TDS.pop());
-    TDS.push( setInterval(ads, (Math.random()*20+10)*1000));
+//    while(TDS.length) clearInterval(TDS.pop());
+//    TDS.push( setInterval(ads, (Math.random()*20+10)*1000));
     ads();
   };
   var sp = [].slice.call(document.getElementsByTagName('script')).filter(function(s){return s.src.indexOf('metrics.js')>-1;})[0];
