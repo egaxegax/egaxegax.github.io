46ab5dc0c8a93547a8c16d090166da7a048d2396
egax <egax@ya.ru>
2024-08-03 01:08:08 +0300

    *ads +inimage

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -71,8 +71,9 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
         case 'inImg':  (function addInImage(images) {
                           if(!images.length) return;
                           const image = images.shift();
+                          image.id = `ya_rtb_${blid}-${Math.random().toString(16).slice(2)}`;
                           console.log(image);
-                          window.yaContextCb.push(()=>{Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-7295044-'+rtbid, renderTo:`ya_rtb_${blid}-${Math.random().toString(16).slice(2)}`, type:'inImage'});});
+                          window.yaContextCb.push(()=>{Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-7295044-'+rtbid, renderTo:image.id, type:'inImage'});});
                           addInImage(images);
                         })(Array.from(document.getElementById('page_content').querySelectorAll('img'))); break;
         case 'widget': window.yaContextCb.push(()=>{Ya.Context.AdvManager.renderWidget({darkTheme:p_dark, blockId:'C-A-7295044-'+rtbid, renderTo:blid});}); break;
