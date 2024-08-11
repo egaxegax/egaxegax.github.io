17f0b1ad751e27d263433f19cee12e0d0fdc87a7
egax <egax@ya.ru>
2024-08-11 17:37:30 +0300

    *updaterss

--- vesti/readrss.py
+++ vesti/readrss.py
@@ -25,8 +25,9 @@ with urllib.request.urlopen("https://habr.com/ru/rss/articles/?fl=ru") as purl:
     for channel in root.findall("channel"):
       gdate = channel.find('pubDate').text
       gdt = time.strptime(gdate, "%a, %d %b %Y %H:%M:%S %Z")
-      gyear = time.strftime('%y', gdt)
-      with open(os.path.join(cdir, str(gdt.tm_year), '{y}{m}{d} 0300.md'.format(y=gyear, m=('%02d' % gdt.tm_mon), d=('%02d' % gdt.tm_mday))), 'w+', encoding='utf-8', newline='\n') as fp:
+      if not os.path.exists(os.path.join(cdir, str(gdt.tm_year))):
+        os.mkdir(os.path.join(cdir, str(gdt.tm_year)))
+      with open(os.path.join(cdir, str(gdt.tm_year), '{y}{m}{d} 0300.md'.format(y=time.strftime('%y', gdt), m=('%02d' % gdt.tm_mon), d=('%02d' % gdt.tm_mday))), 'w+', encoding='utf-8', newline='\n') as fp:
         fp.write("<h3>Свежая подборка статей с Хабра</h3>")
         for item in channel.findall("item"):
           titl = item.find('title').text
@@ -39,8 +40,6 @@ with urllib.request.urlopen("https://habr.com/ru/rss/articles/?fl=ru") as purl:
   <a class="nodecor" href="{phref}">{titl}</a>
 </div>""".format(titl=tr_chars(titl, 70), phref=phref, ph=('%02d' % pdt.tm_hour), pmi=('%02d' % pdt.tm_min))
           print(gdate, text)
-          if not os.path.exists(os.path.join(cdir, str(gdt.tm_year))):
-            os.mkdir(os.path.join(cdir, str(gdt.tm_year)))
           fp.write(text)
           fcount += 1
 
