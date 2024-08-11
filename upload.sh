b07b7e7ea670e186b7d44487e47124b794b3428d
egax <egax@ya.ru>
2024-08-11 11:08:52 +0300

    *vesti +migrate to reg.ru hosting +upload.sh

---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+lftp <<EOF
+set ssl:verify-certificate no
+open ftp://u2774856:Hae1mz2qo59lnAQF@egax.ru
+ls /www/egax.ru
+mirror --reverse --delete -x .git /home/agena/NetBeansProjects/egaxegax.github.io /www/egax.ru 
+mirror --reverse --delete -x .git /home/agena/NetBeansProjects/dbcartajs /www/egax.ru/dbcartajs
+exit
+EOF
