0153b8087766b9490f435014bdfebf095f64b05f
egax <egax@ya.ru>
2024-08-11 17:53:58 +0300

    *upload.sh

--- upload.sh
+++ upload.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+mdir=`dirname $0`
+
 lftp <<EOF
 set ssl:verify-certificate no
 open ftp://u2774856:Hae1mz2qo59lnAQF@egax.ru
 ls /www/egax.ru
-mirror --reverse --delete -x .git /home/agena/NetBeansProjects/egaxegax.github.io /www/egax.ru
-mirror --reverse --delete -x .git /home/agena/NetBeansProjects/dbcartajs /www/egax.ru/dbcartajs
+mirror --reverse --delete -x .git $mdir /www/egax.ru
+mirror --reverse --delete -x .git $mdir/../dbcartajs /www/egax.ru/dbcartajs
 exit
 EOF
