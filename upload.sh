c64e5b7e5d8e369e641d6b39eb168ec3ca92a373
egax <egax@ya.ru>
2024-08-11 17:12:54 +0300

    *upload.sh

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ lftp <<EOF
 set ssl:verify-certificate no
 open ftp://u2774856:Hae1mz2qo59lnAQF@egax.ru
 ls /www/egax.ru
-mirror --reverse --delete -x .git -x __pycache__ /home/agena/NetBeansProjects/egaxegax.github.io /www/egax.ru
+mirror --reverse --delete -x .git /home/agena/NetBeansProjects/egaxegax.github.io /www/egax.ru
 mirror --reverse --delete -x .git /home/agena/NetBeansProjects/dbcartajs /www/egax.ru/dbcartajs
 exit
 EOF
