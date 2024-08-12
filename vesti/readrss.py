a1cc9a07b192eb675e571dd69c3c9a22d04f005a
egax <egax@ya.ru>
2024-08-13 01:09:47 +0300

    *vest rss

--- vesti/readrss.py
+++ vesti/readrss.py
@@ -13,9 +13,11 @@ sys.path.insert(0, cdir+'/..')
 from update import main as update_main
 from updatelist import main as updatelist_main, tr_chars
 
+import locale
+
 RSSlist = {
-  'Свежая подборка статей с Хабра': 'https://habr.com/ru/rss/articles/?fl=ru',
-  'РИА Новости сегодня' :'https://ria.ru/export/rss2/index.xml'
+  'Подборка статей с Хабра': 'https://habr.com/ru/rss/articles/?fl=ru',
+  'РИА Новости' :'https://ria.ru/export/rss2/index.xml'
 }
 fcount = 0
 rcount = 0
@@ -27,22 +29,25 @@ for hdr, url in RSSlist.items():
     if rsstext:
       root = ET.fromstring(rsstext)
       for channel in root.findall("channel"):
-        gdt = time.localtime()
-        if not os.path.exists(os.path.join(cdir, str(gdt.tm_year))):
-          os.mkdir(os.path.join(cdir, str(gdt.tm_year)))
-        with open(os.path.join(cdir, str(gdt.tm_year), '{y}{m}{d} {h}{c}.md'.format(y=time.strftime('%y', gdt), m=('%02d' % gdt.tm_mon), d=('%02d' % gdt.tm_mday), h=('%02d' % gdt.tm_hour), c=('%02d' % rcount))), 'w+', encoding='utf-8', newline='\n') as fp:
-          fp.write('<h3>'+hdr+'</h3>')
+        cdtm = time.localtime()
+        if not os.path.exists(os.path.join(cdir, str(cdtm.tm_year))):
+          os.mkdir(os.path.join(cdir, str(cdtm.tm_year)))
+        with open(os.path.join(cdir, str(cdtm.tm_year), '{y}{m}{d} {h}{c}.md'.format(y=time.strftime('%y', cdtm), m=('%02d' % cdtm.tm_mon), d=('%02d' % cdtm.tm_mday), h=('%02d' % cdtm.tm_hour), c=('%02d' % rcount))), 'w+', encoding='utf-8', newline='\n') as fp:
+          locale.setlocale(locale.LC_ALL, 'Russian')
+          fp.write('<h3>'+hdr+' на '+ time.strftime('%c', cdtm) +'</h3>')
+          locale.setlocale(locale.LC_ALL, 'C')
           for item in channel.findall('item'):
             titl = item.find('title').text
             phref = item.find('link').text
             pdate = item.find('pubDate').text
+            print(pdate)
             pdt = time.strptime(pdate.replace('+0300','MSK'), '%a, %d %b %Y %H:%M:%S %Z')
             text = """
 <div class="rss">
   <span class="smaller gray hspace">{ph}:{pmi}</span>
   <a class="nodecor" href="{phref}">{titl}</a>
 </div>""".format(titl=tr_chars(titl, 70), phref=phref, ph=('%02d' % pdt.tm_hour), pmi=('%02d' % pdt.tm_min))
-            print(gdt, text)
+            print(cdtm, text)
             fp.write(text)
             fcount += 1
 
