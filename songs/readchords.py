b43d7448c0cfb3640f2e37eeb9669dc4329f4906
egaxegax <egaxegax@gmail.com>
2024-09-28 13:45:35 +0300

    *songs++

--- songs/readchords.py
+++ songs/readchords.py
@@ -31,9 +31,8 @@ for fn in glob.glob(path+'/chords*.json'):
         artpath = os.path.join(rpath, d, art)
         if not os.path.exists(artpath):
           os.mkdir(artpath)
-        songpath = os.path.join(artpath, song['details']['title']+'.md')
-        open(songpath, 'w').write(ctime + '\n' + song['text'].replace(']:', ']:\n').replace('\n', '  \n'))
-        # print(song['text'].replace('\n', '  \n'))
+        songpath = os.path.join(artpath, song['details']['title']+'.txt')
+        open(songpath, 'w').write(ctime + '\n' + song['text'].replace(']:', ']:\n'))
         print(songpath)
         fcount += 1
 
