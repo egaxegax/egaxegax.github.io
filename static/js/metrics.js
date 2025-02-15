0b2a9426f96254857891271d08f02ba273c9d0a2
egaxegax <egaxegax@gmail.com>
2025-02-15 14:42:57 +0300

    *ads floor

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -108,7 +108,7 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
     addVkRTB_Block();
   };
   [].slice.call(document.getElementsByTagName('script')).filter(function(s){return s.src.indexOf('metrics.js')>-1;}).map(function(sp){
-    if(sp.getAttribute('data-floor')) addVkRTB_BlockTag(document.head, '1_s');
+    if(sp.getAttribute('data-floor')) addVkRTB_BlockTag(document.head, '0_s');
   });
 }
 { // VK Widget Like
