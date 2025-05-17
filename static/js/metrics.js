fce4941e3117ff3ff0b8fe40c40e222af8b6b4f6
egaxegax <egaxegax@gmail.com>
2025-05-17 20:36:15 +0000

    *metrics ya

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -53,22 +53,22 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
   window.addYaRTB_Block = function(blid,rtbid,typ,p_dark){
     function ads(){ 
       function run(){ console.log(blid,rtbid,typ,p_dark);
-        switch(typ){
-        case 'flMob':  window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, type:'floorAd'});}); break;
-        case 'flDesk': window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, type:'floorAd', platform:'desktop'});}); break;
-        case 'fullMob':  window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, type:'fullscreen', platform:'touch'});}); break;
-        case 'inImg':  (function addInImage(images) {
-                          if(!images.length) return;
-                          const image = images.shift();
-                          image.id = `ya_rtb_${blid}-${Math.random().toString(16).slice(2)}`;
-                          // console.log(image);
-                          window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, renderTo:image.id, type:'inImage'});});
-                          addInImage(images);
-                        })(Array.from(document.getElementById('page_content').querySelectorAll('img'))); break;
-        case 'widget': window.yaContextCb.push(function(){Ya.Context.AdvManager.renderWidget({darkTheme:p_dark, blockId:'C-A-R-A-14075053-'+rtbid, renderTo:blid});}); break;
-        default:       window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, renderTo:blid, type:typ});}); break;
-      }}
-      // run();
+        if(window.YA_RTB[rtbid]) switch(typ){
+          case 'flMob':  window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, type:'floorAd'});}); break;
+          case 'flDesk': window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, type:'floorAd', platform:'desktop'});}); break;
+          case 'fullMob':  window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, type:'fullscreen', platform:'touch'});}); break;
+          case 'inImg':  (function addInImage(images) {
+                            if(!images.length) return;
+                            const image = images.shift();
+                            image.id = `ya_rtb_${blid}-${Math.random().toString(16).slice(2)}`;
+                            // console.log(image);
+                            window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, renderTo:image.id, type:'inImage'});});
+                            addInImage(images);
+                          })(Array.from(document.getElementById('page_content').querySelectorAll('img'))); break;
+          case 'widget': window.yaContextCb.push(function(){Ya.Context.AdvManager.renderWidget({darkTheme:p_dark, blockId:'C-A-R-A-14075053-'+rtbid, renderTo:blid});}); break;
+          default:       window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, renderTo:blid, type:typ});}); break;
+        }
+      }
       if(!blid) run();
       else if(document.getElementById(blid))
         (new IntersectionObserver(function(es){ es.forEach(function(e){ if(e.isIntersecting){ run(); }}); },{threshold:0.9}).observe(document.getElementById(blid)));
