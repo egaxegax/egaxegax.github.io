d62150240ae1cb428bc463d641a3630c7fcf3c29
egaxegax <egaxegax@gmail.com>
2025-05-28 00:22:10 +0300

    *books +widget

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -50,7 +50,7 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
   (function(m,e,t,r,i,k,a){m[i]=m[i]||[];
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0];k.async=1;k.src=r;a.parentNode.insertBefore(k,a);})
    (window, document, 'script', '//yandex.ru/ads/system/context.js', 'yaContextCb');
-  window.YA_RTB = {1:1, 2:2, 3:3, 4:4, 5:1, 6:1, 7:1, /*feed:10, widget:11, inimage:12*/};
+  window.YA_RTB = {1:1, 2:2, 3:3, 4:4, 5:1, 6:1, 7:1, feed:10, widget:5, inimage:12};
   window.YA_TMR = [];
   window.addYaRTB_Block = function(blid,rtbid,typ,p_dark){
     function ads(){ 
@@ -67,7 +67,7 @@ if(!String(window.location).match(/file:|localhost|127.0.0.1/)){
                             window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, renderTo:image.id, type:'inImage'});});
                             addInImage(images);
                           })(Array.from(document.getElementById('page_content').querySelectorAll('img'))); break;
-          case 'widget': window.yaContextCb.push(function(){Ya.Context.AdvManager.renderWidget({darkTheme:p_dark, blockId:'C-A-R-A-14075053-'+rtbid, renderTo:blid});}); break;
+          case 'widget': window.yaContextCb.push(function(){Ya.Context.AdvManager.renderWidget({darkTheme:p_dark, blockId:'C-A-14075053-'+rtbid, renderTo:blid});}); break;
           default:       window.yaContextCb.push(function(){Ya.Context.AdvManager.render({darkTheme:p_dark, blockId:'R-A-14075053-'+rtbid, renderTo:blid, type:typ});}); break;
         }
       }
