5f1b4784de9b90db1c2ff09d5da4dfd00c632d63
egaxegax <egaxegax@gmail.com>
2026-04-04 14:16:24 +0000

    *fetch part js

--- static/js/template.js
+++ static/js/template.js
@@ -31,7 +31,7 @@ async function fetchPart(p, clfunc) {
       const fullBuffer = await response.arrayBuffer();
       clfunc((new TextDecoder('utf-8')).decode(fullBuffer.slice(p.offset, p.offset + p.length + 1)), p, response.status);
     } else if (response.status === 416) {
-      const resp = await fetch( p.url );
+      const resp = await fetch( p.url, { headers: {}} );
       if (response.ok) {
         const fullBuffer = await resp.arrayBuffer();
         clfunc((new TextDecoder('utf-8')).decode(fullBuffer.slice(p.offset, p.offset + p.length + 1)), p, 200);
